3d483f7741a8ff830c23dcb17c1c3de29ee1f047
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-01-29 16:57:00 +0200

    Add header info to all files

---
+++ Challenge1.js
@@ -0,0 +1,20 @@
+/**
+ * @author Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
+ * 
+ * findChildren(santasList, children) compares two list and returns a new list with same elements from both lists
+ * @param {santasList} Santa's List - Santa's list of children in array format
+ * @param {children} Children list - The list of children to be checked.
+ * @returns {newList} List containing every child who appeared on Santa's list.
+ * 
+ * Link to problem: https://kurtosys-prod-eng.atlassian.net/wiki/spaces/GRAD/pages/223871621/Challenge+1+-+Santa+s+Naughty+List
+ */
+
+function findChildren(santasList, children) {
+    // Your code
+}
+
+let santasList = ["Tom","Errol","Sam","Peter","Jennifer"];
+let children = ["Errol", "Peter", "Jennifer"];
+
+santasList = ["Tom","Errol","Sam", "mistyMay","Peter","Jennifer", "macMerphy"];
+children = ["Errol", "Peter", "jennifer", "mistymay", "MacMerphy"];
---
+++ Challenge3.js
@@ -0,0 +1,10 @@
+/**
+ * @author Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
+ * 
+ * findChildren(santasList, children) compares two list and returns a new list with same elements from both lists
+ * @param {santasList} Santa's List - Santa's list of children in array format
+ * @param {children} Children list - The list of children to be checked.
+ * @returns {newList} List containing every child who appeared on Santa's list.
+ * 
+ * Link to problem: https://kurtosys-prod-eng.atlassian.net/wiki/spaces/GRAD/pages/223871621/Challenge+1+-+Santa+s+Naughty+List
+ */
