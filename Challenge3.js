f89dcb0927331d2dd375010d9d24144d4bc19fad
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-01-30 14:01:52 +0200

    Wrapped the code in a function

--- Challenge3.js
+++ Challenge3.js
@@ -1,21 +1,28 @@
 /**
  * @author Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
  * 
- * findChildren(santasList, children) compares two list and returns a new list with same elements from both lists
- * @param {santasList} Santa's List - Santa's list of children in array format
- * @param {children} Children list - The list of children to be checked.
- * @returns {newList} List containing every child who appeared on Santa's list.
+ * showAveTime() uses the 'node-fetch' module to show the average time taken to send a request and received a JSON object
+ * from 'http://date.jsontest.com/'
  * 
  * Link to problem: https://kurtosys-prod-eng.atlassian.net/wiki/spaces/GRAD/pages/223871627/Challenge+3+-+Quick+and+dirty+network+perf+calculator
  */
 
-let fetch = require("node-fetch");
+function showAveTime() {
 
-let time = new Date().getTime();
-fetch('http://date.jsontest.com/').then(function(res) {
-        return res.json();
-    }).then(function(json) {
-        console.log("Time taken: " + (json.milliseconds_since_epoch - time) + " ms");
-        //time = json.milliseconds_since_epoch;
-});
-  
+    let fetch = require("node-fetch");
+    let sum = 0;
+
+    for(let i = 1; i <= 100; i++)
+    {
+        let time = new Date().getTime();
+        fetch('http://date.jsontest.com/').then(function(res) {
+                return res.json();
+            }).then(function(json) {
+                //console.log("Time taken: " + (json.milliseconds_since_epoch - time) + " ms");
+                sum += json.milliseconds_since_epoch - time;
+                console.log("Average time taken to receive date: " + sum/i + " ms");
+        });
+    }
+}
+
+showAveTime();
