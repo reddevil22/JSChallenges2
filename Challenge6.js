f0e27a80209f5e832bc8852e09121232c62123d6
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-01-31 10:51:20 +0200

    Added 'sumValues' method

--- Challenge6.js
+++ Challenge6.js
@@ -8,3 +8,25 @@
  * 
  * Link to problem: https://kurtosys-prod-eng.atlassian.net/wiki/spaces/GRAD/pages/223871621/Challenge+1+-+Santa+s+Naughty+List
  */
+
+let fs = require('fs');
+
+let data = JSON.parse(fs.readFileSync('recursionData.json', 'utf8'));
+
+//console.log(data);
+
+let sum = 0;
+function sumValues(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === "object") {
+            sumValues(obj[key]);
+        } 
+        else {
+            //console.log(obj[key]);
+            sum += obj[key];
+        }
+    }
+    return sum;
+}
+
+console.log(sumValues(data));
