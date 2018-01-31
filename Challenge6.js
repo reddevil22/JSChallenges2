8e7a55a6003a9da2ecc12b41452bb67deca5a41d
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-01-31 10:55:22 +0200

    Modified header info

--- Challenge6.js
+++ Challenge6.js
@@ -1,12 +1,11 @@
 /**
  * @author Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
  * 
- * findChildren(santasList, children) compares two list and returns a new list with same elements from both lists
- * @param {santasList} Santa's List - Santa's list of children in array format
- * @param {children} Children list - The list of children to be checked.
- * @returns {newList} List containing every child who appeared on Santa's list.
+ * sumValues(obj) Recursively adds the 'total' attribute in obj
+ * @param {obj} The nested array to be summmed
+ * @returns {sum} The sum of all the 'total' attributes
  * 
- * Link to problem: https://kurtosys-prod-eng.atlassian.net/wiki/spaces/GRAD/pages/223871621/Challenge+1+-+Santa+s+Naughty+List
+ * Link to problem: https://kurtosys-prod-eng.atlassian.net/wiki/spaces/GRAD/pages/223839039/Challenge+6+-+Recursion
  */
 
 let fs = require('fs');
