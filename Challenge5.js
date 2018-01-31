5db639a9daf880971e21b4b08fd0a0b332ea3bf0
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-01-31 09:54:11 +0200

    Wrapped merging in a function

--- Challenge5.js
+++ Challenge5.js
@@ -1,37 +1,31 @@
 /**
  * @author Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
  * 
- * findChildren(santasList, children) compares two list and returns a new list with same elements from both lists
- * @param {santasList} Santa's List - Santa's list of children in array format
- * @param {children} Children list - The list of children to be checked.
- * @returns {newList} List containing every child who appeared on Santa's list.
+ * mergeTwoArrays(arr1, arr2) merges 2 array of objects based on '_id' key and returns a new array
+ * @param {arr1} First array to be merged
+ * @param {arr2} Second array to be merged
+ * @returns {merged} New array containing merged objects
  * 
  * Link to problem: https://kurtosys-prod-eng.atlassian.net/wiki/spaces/GRAD/pages/223740484/Challenge+5+-+Array+Merge
  */
+
+function mergeTwoArrays(arr1, arr2) {
+    let merged = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if(arr1[i]._id == arr2[j]._id) {
+                merged.push(Object.assign(arr1[i], arr2[j]));
+            }
+        }
+    }
+
+    return merged;
+}
 
 let fs = require('fs');
 
 let data1 = JSON.parse(fs.readFileSync('data1.json', 'utf8'));
 let data2 = JSON.parse(fs.readFileSync('data2.json', 'utf8'));
 
-console.log(data1);
-console.log(data2);
-
-/* let arr = [];
-
-for (let index1 = 0; index1 < data1.length; index1++) {
-
-    let shared = false;
-    for (let index2 = 0; index2 < data2.length; index2++) {
-        console.log(data2[index2]._id + " " + data1[index2]._id);
-        if (data2[index2]._id == data1[index2]._id) {
-            shared = true;
-            break;
-        }
-        
-    }
-    if (!shared) {
-        arr.push(data1[index1]);
-    }
-}
-arr = arr.concat(data2); */
+console.log(mergeTwoArrays(data1, data2));
