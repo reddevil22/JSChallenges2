b6cd0c9371f63892927047967a4921b2e65e130a
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-01-31 08:05:41 +0200

    Created method for reading in JSON files

--- Challenge5.js
+++ Challenge5.js
@@ -6,5 +6,11 @@
  * @param {children} Children list - The list of children to be checked.
  * @returns {newList} List containing every child who appeared on Santa's list.
  * 
- * Link to problem: https://kurtosys-prod-eng.atlassian.net/wiki/spaces/GRAD/pages/223871621/Challenge+1+-+Santa+s+Naughty+List
+ * Link to problem: https://kurtosys-prod-eng.atlassian.net/wiki/spaces/GRAD/pages/223740484/Challenge+5+-+Array+Merge
  */
+
+let fs = require('fs');
+let data1 = fs.readFileSync('data1.json', 'utf8');
+let data2 = fs.readFileSync('data2.json', 'utf8');
+console.log(data1);
+console.log(data2);
